08b87848702a6de162c3c21b34bf4b8be2f30f9f
Top C <136592513+Abdullahciti@users.noreply.github.com>
2024-02-24 23:37:31 +0100

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,12 +1,28 @@
 // Compoenents
 
-const langs = document.querySelector(".languages");
-
 const langSkills = document.querySelectorAll(".languages ul li span");
 let started = false;
+const codeSkills = document.querySelectorAll(".wrapper .exp li span");
+let startedTwo = false;
 
 window.onscroll = () => {
-    if (window.scrollY > 130) {
+      if (window.scrollY > 50) {
+            if (!startedTwo) {
+          let startIndex = 0;
+          codeSkills.forEach((el) => {
+            let interValid = setInterval(() => {
+              el.style.width = `${startIndex++}%`;
+              if (parseInt(startIndex) >= parseInt(el.dataset.width)) {
+                console.log("heelo")
+                clearInterval(interValid);
+              }
+            }, 4000 / parseInt(el.dataset.width));
+          });
+          console.log("heelo")
+        };
+        startedTwo = true;
+      }
+      if (window.scrollY > 130) {
             if (!started) {
           let startIndex = 0;
           langSkills.forEach((span) => {
@@ -20,4 +36,5 @@ window.onscroll = () => {
         };
         started = true;
       }
+
 }
